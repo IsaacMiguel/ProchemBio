003029edb676f22cd4e60ac858268bc6bf382f50
Marcos Iglesias <Markkos89@gmail.com>
2015-05-04 12:04:25 -0300

    this closes #52

--- models/mEnvases.js
+++ models/mEnvases.js
@@ -16,16 +16,16 @@ function getAllActivos(cb){
 	conn("select * from envases where activa = 1 order by nombre", cb);
 }
 
-function insert(codigo, nombre, umed, capacidad, activo, cb){
-	conn("insert into envases(codigo, nombre, umed, capacidad, activa) values('"+codigo+"', '"+nombre+"','"+ umed+"',"+capacidad+","+ activo+")", cb)
+function insert(codigo, nombre, umed, capacidad, uxpallet, activo, cb){
+	conn("insert into envases(codigo, nombre, umed, capacidad, uxpallet, activa) values('"+codigo+"', '"+nombre+"','"+ umed+"',"+capacidad+", "+uxpallet+", "+ activo+")", cb)
 }
 
 function getEnvasePorId(id, cb){
 	conn("select * from envases where id ="+id, cb);
 }
 
-function update(id, codigo, nombre, umed, capacidad, activo, cb){
-	conn("update envases set codigo='"+codigo+"', nombre='"+nombre+"', umed='"+umed+"', capacidad="+capacidad+", activa="+activo+" where id="+id, cb);
+function update(id, codigo, nombre, umed, capacidad, uxpallet, activo, cb){
+	conn("update envases set codigo='"+codigo+"', nombre='"+nombre+"', umed='"+umed+"', capacidad="+capacidad+", uxpallet="+uxpallet+", activa="+activo+" where id="+id, cb);
 }
 
 function del(id, cb){
